d1025c0eefefc8d18c0e53ddb52e849d5eea6d24
chitang233 <bilibilichitang@hotmail.com>
2021-01-01 19:47:26 +0800

    v1.0

---
+++ main.cpp
@@ -0,0 +1,53 @@
+#include <iostream>
+#include <cstring>
+using namespace std;
+
+#define FILL_SIZE 1024 * 1024 * 100
+
+void fuckMemory();
+void fuckMemoryWithoutMemset();
+
+int main()
+{
+	int choose;
+	cout << "Start FuckMemory(1)" << endl << "Start FuckMemory without memset(2)" << endl;
+	cout << endl << "Choose a mode(Default is \"Start FuckMemory\"): ";
+	cin >> choose;
+	switch(choose)
+	{
+		case 1:
+				fuckMemory();
+		case 2:
+				fuckMemoryWithoutMemset();
+		default:
+				fuckMemory();
+	}
+	return 0;
+}
+
+void fuckMemoryWithoutMemset()
+{
+	long long memsize;
+	memsize = 0;
+	while(1)
+	{
+		memsize += FILL_SIZE;
+		char *buf = new char[FILL_SIZE];
+		cout << "Current memsize: " << memsize / 1024 / 1024 << "MB";
+		getchar();
+	}
+}
+
+void fuckMemory()
+{
+	long long memsize;
+	memsize = 0;
+	while(1)
+	{
+		memsize += FILL_SIZE;
+		char *buf = new char[FILL_SIZE];
+		memset (buf,rand()%255, FILL_SIZE);
+		cout << "Current memsize: " << memsize / 1024 / 1024 << "MB";
+		getchar();
+	}
+}
